342d5978e2dfe85a8cfd43c9c1356192fe70c366
ivan <ivan-guita@hotmail.com>
2022-10-19 20:20:46 -0300

    Conceito de componentes e uso de props compreendido e implementado final da aula 05

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
+      <SayMyName nome="ivan" />
+      <Pessoa nome="tiago" idade="36" profissao="vendedor" foto="https://via.placeholder.com/150" />
     </div>
   );
 }
---
+++ meu-projeto/src/components/SayMyName.js
@@ -0,0 +1,10 @@
+function SayMyName(props){
+
+    return(
+
+        <h1>Fala ai {props.nome}, suave ?</h1>
+    )
+
+}
+
+export default SayMyName;
