0fc29fd49bd257aee0df81378c4776da8248e377
Ivan <ivan-guita@hotmail.com>
2022-10-20 14:21:36 -0300

    conceito de prop Types entendido e implementado

--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -8,8 +8,8 @@ function List(){
         <>
           <h2>Minha lista</h2>
           <ul>
-            <Item marca="ferrari"/>
-            <Item marca="fiat"/>
+            <Item marca="ferrari" ano_lancamento={1985}/>
+            <Item marca="fiat" no_lancamento={1964}/>
             
           </ul>
         
